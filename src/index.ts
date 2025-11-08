2546853f281e264be491b38f01cd3f9048e77563
Adrien Pensart <crunchengine@gmail.com>
2025-11-08 13:07:47 -0500

    fixed bug

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,36 @@
 import { DurableObject } from 'cloudflare:workers';
 import { Buffer } from 'node:buffer';
-import { log } from 'node:console';
+import { table } from 'node:console';
 
 class User {
-	name!: string;
-	joinedAt!: number;
-	lastActiveAt!: number;
-	ip!: string;
+	name: string;
+	ready: boolean;
+	joinedAt: number;
+	lastActiveAt: number;
+	ip: string | undefined;
 
-	constructor(name: string, ip: string) {
+	constructor(name: string, ip: string | undefined) {
 		this.name = name;
 		this.joinedAt = Date.now();
 		this.lastActiveAt = this.joinedAt;
 		this.ip = ip;
+		this.ready = false;
 	}
 }
 
 export interface Env {
-	USER: string;
-	PASSWORD: string;
+	ADMIN_USER: string;
+	ADMIN_PASSWORD: string;
 	MY_DURABLE_OBJECT: DurableObjectNamespace<MyDurableObject>;
 }
-const BASIC_USER = 'admin';
-const BASIC_PASS = 'password';
+
+const success = {
+	status: 200,
+	headers: {
+		'Content-Type': 'application/json;charset=utf-8',
+		'Cache-Control': 'no-store',
+	},
+};
 const IP_HEADER = 'CF-Connecting-IP';
 const DEFAULT_TABLE = 'panama';
 type Tables = Map<string, Map<string, User>>;
@@ -63,9 +71,73 @@ export class MyDurableObject extends DurableObject<Env> {
 	}
 	async finish(username: string, ip?: string): Promise<number> {
 		this.tables = (await this.ctx.storage.get('tables')) || new Map();
-		let panamaTable = this.tables.get(DEFAULT_TABLE) || new Map();
+		if (this.tables.size == 0) {
+			return 304;
+		}
 
+		let panamaTable = this.tables.get(DEFAULT_TABLE) || new Map();
 		let foundUserTable: UserAndTable | undefined;
+
+		outerLoop: for (const [tableName, users] of this.tables) {
+			innerLoop: for (const [_, user] of users) {
+				if (user.name != username) {
+					continue innerLoop;
+				}
+				if (ip !== undefined && ip != user.ip) {
+					return 401;
+				}
+
+				console.log(`user ${user.name} found on table ${table}`);
+				foundUserTable = new UserAndTable(tableName, user);
+
+				if (tableName != DEFAULT_TABLE) {
+					user.ready = false;
+					users.delete(username);
+					panamaTable.set(username, user);
+				}
+				break outerLoop;
+			}
+		}
+
+		if (!foundUserTable) {
+			return 404;
+		}
+
+		// don't move panama players
+		if (foundUserTable.table == DEFAULT_TABLE) {
+			return 304;
+		}
+
+		let oldTable = this.tables.get(foundUserTable.table);
+		if (oldTable) {
+			for (let user of oldTable.values()) {
+				user.ready = false;
+				panamaTable.set(user.name, user);
+			}
+			this.tables.set(DEFAULT_TABLE, panamaTable);
+			this.tables.delete(foundUserTable.table);
+		}
+		await this.ctx.storage.put('tables', this.tables);
+		return 200;
+	}
+	async quit(username: string, ip?: string): Promise<number> {
+		let code = await this.finish(username, ip);
+		if (code != 200) {
+			return code;
+		}
+
+		let panamaTable = this.tables.get(DEFAULT_TABLE) || new Map();
+		panamaTable.delete(username);
+		await this.ctx.storage.put('tables', this.tables);
+		return 200;
+	}
+	async ready(username: string, ip?: string): Promise<boolean> {
+		this.tables = (await this.ctx.storage.get('tables')) || new Map();
+		if (this.tables.size == 0) {
+			return false;
+		}
+
+		let found = false;
 		this.tables.forEach((table, tableName) => {
 			let user = table.get(username);
 			if (user === undefined) {
@@ -75,76 +147,15 @@ export class MyDurableObject extends DurableObject<Env> {
 				return;
 			}
 			console.log(`user ${user.name} found on table ${tableName}`);
-			foundUserTable = new UserAndTable(tableName, user);
-
-			if (tableName != DEFAULT_TABLE) {
-				table.delete(username);
-				panamaTable.set(username, user);
-			}
-		});
-
-		if (!foundUserTable) {
-			return 404;
-		}
-		if (foundUserTable.table == DEFAULT_TABLE) {
-			return 304;
-		}
-
-		let oldTable = this.tables.get(foundUserTable.table);
-		if (oldTable) {
-			for (let user of oldTable.values()) {
-				panamaTable.set(user.name, user);
-			}
-			this.tables.set(DEFAULT_TABLE, panamaTable);
-			this.tables.delete(foundUserTable.table);
-		}
-		await this.ctx.storage.put('tables', this.tables);
-		return 200;
-	}
-	async quit(username: string, ip?: string): Promise<boolean> {
-		this.tables = (await this.ctx.storage.get('tables')) || new Map();
-		let found = false;
-		let oldTableName: string = '';
-		this.tables.forEach((table, tableName) => {
-			let user = table.get(username);
-			if (user === undefined) {
-				return;
-			}
-			if (ip !== undefined && ip != user.ip) {
-				return;
-			}
-
 			found = true;
-			if (tableName != DEFAULT_TABLE) {
-				oldTableName = tableName;
-			}
-			table.delete(username);
+			user.ready = true;
 		});
-
-		if (!found) {
-			return false;
-		}
-
-		if (oldTableName.length === 0) {
+		if (found) {
 			await this.ctx.storage.put('tables', this.tables);
-		} else {
-			let oldTable = this.tables.get(oldTableName);
-			if (oldTable === undefined) {
-				await this.ctx.storage.put('tables', this.tables);
-			} else {
-				let panamaTable = this.tables.get(DEFAULT_TABLE) || new Map();
-				for (let user of oldTable.values()) {
-					panamaTable.set(user.name, user);
-				}
-
-				this.tables.set(DEFAULT_TABLE, panamaTable);
-				this.tables.delete(oldTableName);
-				await this.ctx.storage.put('tables', this.tables);
-			}
 		}
-		return true;
+		return found;
 	}
-	async join(username: string, ip: string): Promise<boolean> {
+	async join(username: string, ip: string | undefined): Promise<boolean> {
 		this.tables = (await this.ctx.storage.get('tables')) || new Map();
 
 		let existed = false;
@@ -187,12 +198,28 @@ export class MyDurableObject extends DurableObject<Env> {
 		}
 		await this.ctx.storage.put('tables', this.tables);
 	}
-	async getTables(): Promise<string> {
+	async tableReady(tableName: string): Promise<boolean> {
 		this.tables = (await this.ctx.storage.get('tables')) || new Map();
-		console.log('Tables:', this.tables);
-		let pretty = JSON.stringify(Object.fromEntries(this.tables), replacer, 2);
-		console.log('Pretty Tables:', pretty);
-		return pretty;
+		if (this.tables.size == 0) {
+			return false;
+		}
+
+		const table = this.tables.get(tableName);
+		if (!table || table.size == 0) {
+			console.log(`table ${tableName} not found`);
+			return false;
+		}
+		let ready = false;
+		for (const [_, user] of table) {
+			if (!user.ready) {
+				user.ready = true;
+				ready = true;
+			}
+		}
+		if (ready) {
+			await this.ctx.storage.put('tables', this.tables);
+		}
+		return ready;
 	}
 	async deleteTables(): Promise<boolean> {
 		this.tables = (await this.ctx.storage.get('tables')) || new Map();
@@ -209,54 +236,70 @@ export class MyDurableObject extends DurableObject<Env> {
 			return false;
 		}
 
-		let users: User[] = [];
+		let readyUsers: User[] = [];
+		let notReadyUsers: User[] = [];
 		this.tables.forEach((table, _) => {
 			table.forEach((user, _) => {
-				users.push(user);
+				if (user.ready) {
+					readyUsers.push(user);
+				} else {
+					notReadyUsers.push(user);
+				}
 			});
 		});
 
-		users = shuffleArray(users);
+		readyUsers = shuffleArray(readyUsers);
 		this.tables = new Map();
 		await this.ctx.storage.put('tables', this.tables);
 
 		let tableIndex = 1;
-		while (users.length > 0) {
-			if (users.length == 7) {
-				let tableDe7 = users.splice(0, 7);
+		while (readyUsers.length > 0) {
+			if (readyUsers.length == 7) {
+				let tableDe7 = readyUsers.splice(0, 7);
 				await this.assignTable('table-de-7', tableDe7);
 				break;
-			} else if (users.length == 6) {
-				let tableDe6 = users.splice(0, 6);
+			} else if (readyUsers.length == 6) {
+				let tableDe6 = readyUsers.splice(0, 6);
 				await this.assignTable('table-de-6', tableDe6);
 				break;
-			} else if (users.length == 5) {
-				let tableDe5 = users.splice(0, 5);
+			} else if (readyUsers.length == 5) {
+				let tableDe5 = readyUsers.splice(0, 5);
 				await this.assignTable('table-de-5', tableDe5);
 				break;
-			} else if (users.length == 3) {
-				let tableDe3 = users.splice(0, 3);
+			} else if (readyUsers.length == 3) {
+				let tableDe3 = readyUsers.splice(0, 3);
 				await this.assignTable(DEFAULT_TABLE, tableDe3);
 				break;
-			} else if (users.length == 2) {
-				let tableDe2 = users.splice(0, 2);
+			} else if (readyUsers.length == 2) {
+				let tableDe2 = readyUsers.splice(0, 2);
 				await this.assignTable(DEFAULT_TABLE, tableDe2);
 				break;
-			} else if (users.length == 1) {
-				let tableDe1 = users.splice(0, 1);
+			} else if (readyUsers.length == 1) {
+				let tableDe1 = readyUsers.splice(0, 1);
 				await this.assignTable(DEFAULT_TABLE, tableDe1);
 				break;
 			}
-			let tableDe4 = users.splice(0, 4);
+			let tableDe4 = readyUsers.splice(0, 4);
 			await this.assignTable(`table-${tableIndex}`, tableDe4);
 			tableIndex++;
 		}
+
+		await this.assignTable(DEFAULT_TABLE, notReadyUsers);
 		return true;
 	}
 	async clearDo(): Promise<void> {
 		await this.ctx.storage.deleteAlarm();
 		await this.ctx.storage.deleteAll();
 		this.tables = new Map();
+	}
+
+	// no modifying
+	async getTables(): Promise<string> {
+		this.tables = (await this.ctx.storage.get('tables')) || new Map();
+		console.log('Tables:', this.tables);
+		let pretty = JSON.stringify(Object.fromEntries(this.tables), replacer, 2);
+		console.log('Pretty Tables:', pretty);
+		return pretty;
 	}
 	async notifyAll(reason: string) {
 		this.sessions.forEach((_, session) => {
@@ -279,12 +322,13 @@ export class MyDurableObject extends DurableObject<Env> {
 export default {
 	async fetch(request: Request, env: Env, _ctx): Promise<Response> {
 		const url = new URL(request.url);
-		const success = {
-			status: 200,
-			headers: { 'Content-Type': 'application/json;charset=utf-8', 'Cache-Control': 'no-store' },
-		};
-		const internalError = new Response(`internal error`, { status: 500 });
 		const stub = env.MY_DURABLE_OBJECT.getByName('belote');
+		const internalError = new Response(`internal error`, { status: 500 });
+		const unauthorizedError = new Response(`unauthorized`, { status: 401 });
+		const missingUsername = new Response('missing username', { status: 400 });
+		const unchanged = new Response(null, { status: 304 });
+		const ip = request.headers.get(IP_HEADER) || 'unknown';
+		const username = url.searchParams.get('username');
 		if (!stub) {
 			return new Response('Durable Object not found', { status: 500 });
 		}
@@ -296,30 +340,38 @@ export default {
 			}
 
 			// for users
-			case '/me/join':
-				const username = url.searchParams.get('username');
+			case '/me/ready': {
 				if (!username) {
-					return new Response('Missing username', { status: 400 });
+					return missingUsername;
 				}
-				const ip = request.headers.get(IP_HEADER) || 'unknown';
-				await stub.join(username, ip);
-				await stub.notifyAll(`user ${username} joined the Meltdown`);
-				return new Response('🎉 User joined!', success);
-			case '/me/meltdown': {
-				const username = url.searchParams.get('username');
+				if (await stub.ready(username, ip)) {
+					await stub.notifyAll(`user ${username} ready!`);
+				}
+				return new Response('🎉 User ready!', success);
+			}
+			case '/me/join': {
 				if (!username) {
-					return new Response('Missing username', { status: 400 });
+					return missingUsername;
+				}
+				const join = await stub.join(username, ip);
+				if (join) {
+					await stub.notifyAll(`user ${username} joined the Meltdown`);
+					return new Response('🎉 User joined!', success);
+				}
+				return unchanged;
+			}
+			case '/me/meltdown': {
+				if (!username) {
+					return missingUsername;
 				}
 				const response = stub.fetch(request);
 				console.log(`user ${username} connected to Meltdown room`);
 				return response;
 			}
 			case '/me/quit': {
-				const username = url.searchParams.get('username');
 				if (!username) {
-					return new Response('Missing username', { status: 400 });
+					return missingUsername;
 				}
-				const ip = request.headers.get(IP_HEADER) || 'unknown';
 				const quit = await stub.quit(username, ip);
 				if (quit) {
 					await stub.notifyAll(`user ${username} quit the Meltdown`);
@@ -329,17 +381,17 @@ export default {
 				}
 			}
 			case '/me/finish': {
-				const username = url.searchParams.get('username');
 				if (!username) {
-					return new Response('Missing username', { status: 400 });
+					return missingUsername;
 				}
-				const ip = request.headers.get(IP_HEADER) || 'unknown';
 				const code = await stub.finish(username, ip);
 				switch (code) {
+					case 401:
+						return unauthorizedError;
 					case 404:
-						return new Response(`User ${username} not found or not authorized`, { status: 404 });
+						return new Response(`User ${username} not found`, { status: 404 });
 					case 304:
-						return new Response(null, { status: 304 });
+						return unchanged;
 					case 200:
 						await stub.notifyAll(`user ${username} and its friends at the same table finished their game`);
 						return new Response(`🎉 User ${username} moved!`, success);
@@ -348,18 +400,38 @@ export default {
 				}
 			}
 
-			// NEEDS AUTH
-			case '/admin/notify': {
+			// ADMIN with username param
+			case '/admin/users/join': {
 				return authenticate(request, env, async () => {
-					await stub.notifyAll('force notify all');
-					return new Response(`🎉 Users notified!`, success);
+					if (!username) {
+						return missingUsername;
+					}
+					const join = await stub.join(username, undefined);
+					if (join) {
+						await stub.notifyAll(`user ${username} joined`);
+						return new Response(`🎉 User ${username} joined!`, success);
+					}
+					return unchanged;
+				});
+			}
+			case '/admin/users/ready': {
+				return authenticate(request, env, async () => {
+					if (!username) {
+						return missingUsername;
+					}
+					const ready = await stub.ready(username, undefined);
+					if (ready) {
+						await stub.notifyAll(`user ${username} ready`);
+						return new Response(`🎉 User ${username} ready!`, success);
+					} else {
+						return new Response(`User ${username} not found`, { status: 404 });
+					}
 				});
 			}
 			case '/admin/users/delete': {
 				return authenticate(request, env, async () => {
-					const username = url.searchParams.get('username');
 					if (!username) {
-						return new Response('Missing username', { status: 400 });
+						return missingUsername;
 					}
 					const deleted = await stub.quit(username, undefined);
 					if (deleted) {
@@ -372,16 +444,15 @@ export default {
 			}
 			case '/admin/users/finish': {
 				return authenticate(request, env, async () => {
-					const username = url.searchParams.get('username');
 					if (!username) {
-						return new Response('Missing username', { status: 400 });
+						return missingUsername;
 					}
 					const code = await stub.finish(username, undefined);
 					switch (code) {
 						case 404:
 							return new Response(`User ${username} not found`, { status: 404 });
 						case 304:
-							return new Response(null, { status: 304 });
+							return unchanged;
 						case 200:
 							await stub.notifyAll(`user ${username} finished its game`);
 							return new Response(`🎉 User ${username} finished its game!`, success);
@@ -390,15 +461,37 @@ export default {
 					}
 				});
 			}
+
+			// ADMIN without username param
+			case '/admin/notify': {
+				return authenticate(request, env, async () => {
+					await stub.notifyAll('force notify all');
+					return new Response(`🎉 Users notified!`, success);
+				});
+			}
 			case '/admin/users/fixtures': {
 				return authenticate(request, env, async () => {
-					const ip = request.headers.get(IP_HEADER) || 'unknown';
 					const fixtureUsers = ['alice', 'bob', 'carol', 'dave', 'eve', 'frank', 'grace', 'heidi', 'ivan', 'judy', 'seb'];
 					for (let username of fixtureUsers) {
-						await stub.join(username, ip);
+						await stub.join(username, undefined);
 					}
 					await stub.notifyAll('fixtures loaded');
 					return new Response('🎉 Fixture users loaded!', success);
+				});
+			}
+			case '/admin/tables/ready': {
+				return authenticate(request, env, async () => {
+					const table = url.searchParams.get('table');
+					if (!table) {
+						return new Response('missing table name', { status: 400 });
+					}
+					console.log(table);
+					const ready = await stub.tableReady(table);
+					if (!ready) {
+						return unchanged;
+					}
+					await stub.notifyAll('table ready');
+					return new Response('🎉 table ready!', success);
 				});
 			}
 			case '/admin/tables/clear': {
@@ -443,8 +536,8 @@ async function authenticate(request: Request, env: Env, operation: () => Promise
 		headers: { 'WWW-Authenticate': 'Basic realm="my scope", charset="UTF-8"' },
 	});
 
-	const realUser = env.USER ?? BASIC_USER;
-	const realPassword = env.PASSWORD ?? BASIC_PASS;
+	const realUser = env.ADMIN_USER;
+	const realPassword = env.ADMIN_PASSWORD;
 	const authorization = request.headers.get('Authorization');
 	if (!authorization) {
 		return mustLoginResponse;
