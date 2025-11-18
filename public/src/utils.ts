847f7fe40e9a3c8422f6528139821f72c42759e3
Paul HUBERT <paulohubert@hotmail.fr>
2025-11-17 23:40:35 -0500

    Update code

--- public/src/utils.ts
+++ public/src/utils.ts
@@ -2,18 +2,3 @@ function userToString() {
 	console.log("userToString")
 }
 
-class User {
-	name: string;
-	ready: boolean;
-	joinedAt: number;
-	lastActiveAt: number | undefined;
-	ip: string | undefined;
-
-	constructor(name: string, ip: string | undefined) {
-		this.name = name;
-		this.joinedAt = Date.now();
-		this.lastActiveAt = this.joinedAt;
-		this.ip = ip;
-		this.ready = false;
-	}
-}
